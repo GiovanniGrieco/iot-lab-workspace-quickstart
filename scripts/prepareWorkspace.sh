e40604f420bf8992a5f6ec1ffc8c16dc1fb9ee56
Giovanni Grieco <giovanni@grieco.dev>
2020-11-03 10:16:49 +0100

    Updated workspace provision script

--- scripts/prepareWorkspace.sh
+++ scripts/prepareWorkspace.sh
@@ -2,8 +2,47 @@
 
 apt update
 apt upgrade -y
+apt remove -y \
+    snapd
 apt install -y \
     git \
-    virtualenv
+    python3-paho-mqtt \
+    python3-pip \
+    python3-requests
+apt autoremove -y
 
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash -y
+IS_AZURE_CLI_INSTALLED=$(apt show azure-cli | grep APT-Manual-Installed)
+if [ -z "$IS_AZURE_CLI_INSTALLED" ]; then
+    curl -sL https://aka.ms/InstallAzureCLIDeb | bash -y
+fi
+
+
+USER_DIR=$(getent passwd 1000 | cut -d ':' -f 6)
+git clone https://github.com/GiovanniGrieco/iot-lab-data-visualization-example.git \
+    ${USER_DIR}/iot-lab-data-visualization-example/
+
+mkdir -p ${USER_DIR}/device-simulator/opendata
+cat <<EOF >>${USER_DIR}/device-simulator/azure.pem
+-----BEGIN CERTIFICATE-----
+MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ
+RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD
+VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX
+DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y
+ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy
+VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr
+mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr
+IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK
+mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu
+XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy
+dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye
+jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1
+BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3
+DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92
+9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx
+jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0
+Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz
+ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS
+R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp
+-----END CERTIFICATE-----
+EOF
+
